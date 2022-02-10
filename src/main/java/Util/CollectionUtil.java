ad74bcf85e94751534888e66a854a83372de3201
Hieu <Thanglong@gmail.com>
2022-02-10 11:02:42 +0700

    update collectionUtil

--- src/main/java/Util/CollectionUtil.java
+++ src/main/java/Util/CollectionUtil.java
@@ -4,6 +4,6 @@ import java.util.Collection;
 
 public class CollectionUtil {
     public static boolean isNullOrEmpty(Collection collection) {
-        return collection!=null && collection.isEmpty();
+        return collection==null || collection.isEmpty();
     }
 }
